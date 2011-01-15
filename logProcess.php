8a2d26662418a4592ad3c45a71a0ceeabd118ab9
Patrick McGuire <insidenothing@gmail.com>
2011-01-15 10:34:48 -0800

    minor notes

--- logProcess.php
+++ logProcess.php
@@ -1,12 +1,12 @@
 <?php
-mysql_connect();
-mysql_select_db('core');
+mysql_connect(); //connect to database (pconnect?)
+mysql_select_db('apache'); // select database
 $stdin = fopen ('php://stdin', 'r');
 ob_implicit_flush (true); // Use unbuffered output
 while ($line = fgets ($stdin))
 {
-//print $line;
-//mail('service@mdwestserve.com',$line,$line);
-@mysql_query("insert into apacheErrors (message) values ('$line')");
+//print $line; //uncomment to pipe output to default error log
+//mail('insidenothing@gmail.com','Apache Error',$line); //uncomment to send email containing error
+@mysql_query("insert into apacheErrors (message) values ('$line')"); //uncomment to record to database
 }
 ?>
