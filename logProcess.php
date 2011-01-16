f7b844827e4323c3ab86d1ec1ff72e84074cc4da
Patrick McGuire <insidenothing@gmail.com>
2011-01-16 06:23:17 -0800

    no additional line break needed for error_log

--- logProcess.php
+++ logProcess.php
@@ -5,7 +5,7 @@ $stdin = fopen ('php://stdin', 'r');
 ob_implicit_flush (true); // Use unbuffered output
 while ($line = fgets ($stdin))
 {
-error_log($line." \n",3,"/var/log/httpd/error_log"); //uncomment to write to default error log (centos5)
+error_log($line,3,"/var/log/httpd/error_log"); //uncomment to write to default error log (centos5)
 //mail('insidenothing@gmail.com','Apache Error',$line); //uncomment to send email containing error
 @mysql_query("insert into apacheErrors (message) values ('$line')"); //uncomment to record to database
 }
