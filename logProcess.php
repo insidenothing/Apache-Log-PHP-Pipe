7dd22f5e54f569203a8f5bd53db1ad454c689d0f
Patrick McGuire <insidenothing@gmail.com>
2011-01-16 11:42:38 -0800

    starting a configuration

--- logProcess.php
+++ logProcess.php
@@ -1,12 +1,29 @@
-<?php
-mysql_connect(); //connect to database (pconnect?)
-mysql_select_db('apache'); // select database
+<?PHP
+// Configuration 
+$database = "ON"; // (ON/OFF)
+$hardlog = "ON"; // (ON/OFF)
+$email = "OFF"; // (ON/OFF)
+$logfile = "/var/log/httpd/error_log"; // full path to error log (make sure apache can write to log)
+$email_subject = "Apache Error at ".time(); // subject with time since the Unix Epoch appended
+$email_to = ""; // email address to send error to
+
+// Application
+if($database == "ON"){
+ mysql_connect(); //connect to database
+ mysql_select_db('apache'); // select database
+}
 $stdin = fopen ('php://stdin', 'r');
 ob_implicit_flush (true); // Use unbuffered output
 while ($line = fgets ($stdin))
 {
-error_log($line,3,"/var/log/httpd/error_log"); //uncomment to write to default error log (centos5)
-//mail('insidenothing@gmail.com','Apache Error',$line); //uncomment to send email containing error
-@mysql_query("insert into apacheErrors (message) values ('$line')"); //uncomment to record to database
+ if($hardlog == "ON" && file_exists($logfile)){ 
+  error_log($line,3,$logfile);
+ } 
+ if($email == "ON" && $email_to && $email_subject){
+  mail($email_to,$email_subject,$line); 
+ }
+ if($database == "ON"){
+  @mysql_query("insert into apacheErrors (message) values ('$line')"); 
+ }
 }
 ?>
